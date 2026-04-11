754cffdbd9192401993afd2015f4fddd5fbd5e73
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-04-11 23:18:22 +0600

    create app

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to the Blog Home Page!")
+
+def about(request):
+    return HttpResponse("This is the About Page of the Blog.")
+   
