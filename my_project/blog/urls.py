381e8763beecd2d70f507864d9856fd7c6513824
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-04-17 21:44:09 +0600

    kwargs

--- my_project/blog/urls.py
+++ my_project/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
    path('user/<str:username>/', views.user_profile, name='user_profile'),
 
-   path('article/<int:year>/<int:month>/', views.article_detail, name='article_detail'),
+   path('article/<int:year>/<int:month>/<int:day>/', views.article_detail, name='article_detail'),
 
    re_path(r'^article/(?P<year>[0-9]{4})/$', views.article_year, name='article_year'),
     
--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -20,5 +20,10 @@ def user_profile(request, username):
 def article_year(request, year):
     return HttpResponse(f"This is the article archive for the year: {year}")
 
-def article_detail(request, year, month):
-    return HttpResponse(f"<h1>This is the article detail for {year}-{month}</h1>")
+# def article_detail(request, year, month):
+#     return HttpResponse(f"<h1>This is the article detail for {year}-{month}</h1>")
+
+#keyword arguements
+def article_detail(request,**kwargs):
+    
+    return HttpResponse(f"<h1>This is the article detail for {kwargs['year']}_{kwargs['month']}_{kwargs['day']}</h1>")
